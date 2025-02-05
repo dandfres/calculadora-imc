2519c9f214e11591206f1112147b2473638e76b3
Sebastian <sebas00tiam@gmail.com>
2025-02-04 22:04:19 -0500

    Mejorada la validacion de datos para evitar errores.

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
-let wight = parseFloat(prompt("Enter your weight in kg:", "70"));
-let height = parseFloat(prompt("Enter your height in meters:", "1.75"));
+let weight = parseFloat(prompt("Ingresa tu peso en kg:", "70"));
+let height = parseFloat(prompt("Ingresa tu estatura en metros:", "1.75"));
 
-let imc = wight / (height ** 2);
 let result = document.getElementById("demo");
 let imgDOM = document.getElementById("img");
-// alert(`Your IMC is ${imc.toFixed(2)}`);
-if (imc < 18.5) {
-    result.innerText = `Tú IMC es: ${imc.toFixed(2)} y estás bajo de peso !!`;
-    imgDOM.src = "images/underweight.png";
-} else if (imc >= 18.5 && imc < 24.9) {
-    result.innerText = `Tú IMC es: ${imc.toFixed(2)} y estás en tu peso ideal !!`;
-    imgDOM.src = "images/normal-weight.png";
-} else if (imc >= 25 && imc < 29.9) {
-    result.innerText = `Tú IMC es: ${imc.toFixed(2)} y tienes sobrepeso !!`;
-    imgDOM.src = "images/overweight.png";
-} else if (imc >= 30 && imc < 34.9) {
-    result.innerText = `Tú IMC es: ${imc.toFixed(2)}, ESTÁS OBESO !!`;
-    imgDOM.src = "images/obese.png";
+
+if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
+    result.innerText = "Peso o altura no válidos";
+    imgDOM.src = "https://i.redd.it/ux6qsyipx2l61.png";
 } else {
-    result.innerText = `Peso o altura no válidos`;
+    let imc = weight / (height ** 2);
+    if (imc < 18.5) {
+        result.innerText = `Tú IMC es: ${imc.toFixed(2)} y estás bajo de peso !!`;
+        imgDOM.src = "images/underweight.png";
+    } else if (imc >= 18.5 && imc < 24.9) {
+        result.innerText = `Tú IMC es: ${imc.toFixed(2)} y estás en tu peso ideal !!`;
+        imgDOM.src = "images/normal-weight.png";
+    } else if (imc >= 25 && imc < 29.9) {
+        result.innerText = `Tú IMC es: ${imc.toFixed(2)} y tienes sobrepeso !!`;
+        imgDOM.src = "images/overweight.png";
+    } else if (imc >= 30 && imc < 34.9) {
+        result.innerText = `Tú IMC es: ${imc.toFixed(2)}, ESTÁS OBESO !!`;
+        imgDOM.src = "images/obese.png";
+    } else {
+        result.innerText = `Tú IMC es: ${imc.toFixed(2)}, y estás en obesidad mórbida !!`;
+        imgDOM.src = "images/morbidly-obese.png";
+    }
 }
